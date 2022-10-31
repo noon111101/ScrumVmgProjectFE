5adb3848eabc8b788febf59f8b295ad6cec35c31
noon111101 <trieu11112001@gmail.com>
2022-10-31 16:15:14 +0700

    feat : Report Author

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 import Login from './views/Login.vue';
 import Register from './views/ManageUser.vue';
 
@@ -10,11 +9,6 @@ Vue.use(Router);
 export const router = new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/login',
       component: Login
