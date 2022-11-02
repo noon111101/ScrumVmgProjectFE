4bfcfbd423fdd0ef0e17f4f4381bb085bc828dfd
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2022-11-02 11:07:59 +0700

    Chỉnh sửa sweet alert

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-xl-10">
@@ -8,65 +7,67 @@
             <div class="col-lg-6">
               <div class="card-body p-md-5 mx-md-4">
                 <div class="text-center">
-                  <img src="http://danhbaict.vn/uploads/images/vmg%20logo.jpg"
-                    style="width: 185px;" alt="logo">
+                  <img
+                      src="http://danhbaict.vn/uploads/images/vmg%20logo.jpg"
+                      style="width: 185px"
+                      alt="logo"
+                  />
                   <h4 class="mt-1 mb-5 pb-1">PHÁT TRIỂN CÙNG CÔNG NGHỆ</h4>
                 </div>
                 <form name="form" @submit.prevent="handleLogin">
                   <p>Please login to your account</p>
                   <div class="form-outline mb-4">
-                    <input  placeholder="Enter Your Email"  v-model="user.username"
-            v-validate="{required:true}"
-            type="email"
-            class="form-control"
-            name="username"
-            />
+                    <input
+                        placeholder="Enter Your Email"
+                        v-model="user.username"
+                        v-validate="{ required: true }"
+                        type="email"
+                        class="form-control"
+                        name="username"
+                    />
                     <!-- <label class="form-label" for="form2Example11">Email</label> -->
                   </div>
                   <div
-            v-if="errors.has('username')"
-            class="alert alert-danger"
-            role="alert"
-          >Email is required and correct email format!</div>
+                      v-if="errors.has('username')"
+                      class="alert alert-danger"
+                      role="alert"
+                  >
+                    Email is required and correct email format!
+                  </div>
                   <div class="form-outline mb-4">
-                    <input placeholder="Enter Your Password" v-model="user.password"
-            v-validate="'required'"
-            type="password"
-            class="form-control"
-            name="password"
-            />
+                    <input
+                        placeholder="Enter Your Password"
+                        v-model="user.password"
+                        v-validate="{ required: true }"
+                        type="password"
+                        class="form-control"
+                        name="password"
+                    />
                     <!-- <label class="form-label" for="form2Example22">Password</label> -->
                   </div>
-                           <div
-            v-if="errors.has('password')"
-            class="alert alert-danger"
-            role="alert"
-          >Password is required!</div>
+                  <div
+                      v-if="errors.has('password')"
+                      class="alert alert-danger"
+                      role="alert"
+                  >
+                    Password is required!
+                  </div>
                   <div class="text-center pt-1 mb-5 pb-1">
-
                     <button
-                      class="btn btn-danger btn-block"
-                      :disabled="loading"
+                        class="btn btn-danger btn-block"
+                        :disabled="loading"
                     >
                       Log in
                     </button>
                     <br />
                     <a class="text-muted" href="http://localhost:8081/forgotPassword">Forgot password?</a>
-
-                    <button class="btn btn-danger btn-block" :disabled="loading">Log
-                      in</button>
-                      <br>
-                    <a class="text-muted" href="#!">Forgot password?</a>
-
                   </div>
                 </form>
-
               </div>
             </div>
             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">THAY ĐỔI TẠO KHÁC BIỆT</h4>
-                <p class="small mb-0">VMG khẳng định trong kỷ nguyên số, sở hữu trí tuệ và sự sáng tạo là những tài sản quý giá nhất đối với Tập đoàn</p>
+                <img width="450px" src="../assets/Login.png" />
               </div>
             </div>
           </div>
@@ -77,104 +78,78 @@
 </template>
 
 <script>
-import User from '../models/user';
+import User from "../models/user";
 export default {
-  name: 'LoginVue',
+  name: "LoginVue",
   data() {
     return {
-      user: new User('', ''),
+      user: new User("", ""),
       loading: false,
-      message: ''
+      message: "",
     };
   },
   computed: {
     loggedIn() {
       return this.$store.state.auth.status.loggedIn;
-    }
+    },
   },
   created() {
+    this.$router.push("/login");
     if (this.loggedIn) {
-      this.$router.push('/calender');
+      this.$router.push("/profile");
     }
   },
   methods: {
     handleLogin() {
       this.loading = true;
-      this.$validator.validateAll().then(isValid => {
+      this.$validator.validateAll().then((isValid) => {
         if (!isValid) {
           this.loading = false;
           return;
         }
         if (this.user.username && this.user.password) {
-          this.$store.dispatch('auth/login', this.user).then(
-            () => {
-              this.$router.push('/calender');
-            },
-            error => {
-              this.loading = false;
-              this.message =
-                (error.response && error.response.data && error.response.data.message) ||
-                error.message ||
-                error.toString();
-            }
+          this.$store.dispatch("auth/login", this.user).then(
+              () => {
+                this.$router.push("/profile");
+              },
+              (error) => {
+                this.loading = false;
+                this.message =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+              }
           );
         }
       });
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-/* label {
-  display: block;
-  margin-top: 10px;
-}
-.card-container.card {
-  max-width: 350px !important;
-  padding: 40px 40px;
-}
-.card {
-  background-color: #f7f7f7;
-  padding: 20px 25px 30px;
-  margin: 0 auto 25px;
-  margin-top: 50px;
-  -moz-border-radius: 2px;
-  -webkit-border-radius: 2px;
-  border-radius: 2px;
-  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-}
-.profile-img-card {
-  width: 96px;
-  height: 96px;
-  margin: 0 auto 10px;
-  display: block;
-  -moz-border-radius: 50%;
-  -webkit-border-radius: 50%;
-  border-radius: 50%;
-} */
 .gradient-custom-2 {
-/* fallback for old browsers */
-background: #fccb90;
+  /* fallback for old browsers */
+  /* background: #fccb90; */
 
-/* Chrome 10-25, Safari 5.1-6 */
-background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+  /* Chrome 10-25, Safari 5.1-6 */
+  /* background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593); */
 
-/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  /* background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593); */
 }
 
 @media (min-width: 768px) {
-.gradient-form {
-height: 100vh !important;
-}
+  .gradient-form {
+    height: 100vh !important;
+  }
 }
 @media (min-width: 769px) {
-.gradient-custom-2 {
-border-top-right-radius: .3rem;
-border-bottom-right-radius: .3rem;
-}
+  .gradient-custom-2 {
+    border-top-right-radius: 0.3rem;
+    border-bottom-right-radius: 0.3rem;
+  }
 }
 </style>
