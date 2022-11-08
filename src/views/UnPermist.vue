020ff0c130b2b05f3cd0305478a3ba3dd9c98bfd
thanhpdhe150023@fpt.edu.vn <108501377+thanh23102001@users.noreply.github.com>
2022-11-08 09:58:16 +0700

    thanh

---
+++ src/views/UnPermist.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>
+    You don't have permistion to access this page
+  </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UnPermist"
+}
+</script>
+
+<style scoped>
+</style>
